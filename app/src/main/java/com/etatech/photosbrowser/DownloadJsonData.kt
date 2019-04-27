aa3b664f4b1bda1d1ca930547b57cac090224fa8
fadyfouad <fady.fouad.a@gmail.com>
2019-04-28 00:34:17 +0200

    Initial commit

--- app/src/main/java/com/etatech/photosbrowser/DownloadJsonData.kt
+++ app/src/main/java/com/etatech/photosbrowser/DownloadJsonData.kt
@@ -1,4 +1,45 @@
 package com.etatech.photosbrowser
 
-class DownloadJsonData {
+import android.os.AsyncTask
+import java.io.IOException
+import java.net.MalformedURLException
+import java.net.URL
+
+class DownloadJsonData : AsyncTask<String,Void,String>() {
+
+    private val TAG = "DownloadJsonData"
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun doInBackground(vararg params: String?): String {
+
+        if(params[0]==null){
+            downloadStatus = DownloadStatus.NOT_INSIALIZED
+            return "No Url"
+        }
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        }catch (e : Exception){
+            val errorMessage = when (e){
+                is MalformedURLException -> {
+                    downloadStatus =DownloadStatus.NOT_INSIALIZED
+                    "Invaled URL ${e.message}"
+                }is IOException -> {
+                    downloadStatus =DownloadStatus.FAILED_OR_EMPTY
+                    "Reading Data ${e.message}"
+                }is SecurityException -> {
+                    downloadStatus =DownloadStatus.PERMISSION_ERROR
+                    "Need Permission ${e.message}"
+                }else ->{
+                    downloadStatus = DownloadStatus.ERROR
+                    "Unkown Error"
+                }
+            }
+            return errorMessage
+        }
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
 }
--- app/src/main/java/com/etatech/photosbrowser/DownloadStatus.java
+++ app/src/main/java/com/etatech/photosbrowser/DownloadStatus.java
@@ -1,4 +1,9 @@
 package com.etatech.photosbrowser;
 
 public enum DownloadStatus {
+
+    OK,IDLE,NOT_INSIALIZED,FAILED_OR_EMPTY,PERMISSION_ERROR,ERROR
+
+
+
 }
